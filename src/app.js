5b5f5f1db1aee1872d696c5ae077e6059db71dee
Elliott-Greaves <greaveselliott@me.com>
2017-11-04 20:59:05 +0000

    added component boiler plate

--- src/app.js
+++ src/app.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import configureStore from './redux/store';
 import './app.scss';
-import ScoreBoard from './components/score-board';
-import StartGame from './components/start-game';
-import Board from './components/board';
-import AI from './components/ai';
 import Modernizr from 'modernizr';
 
 const store = configureStore();
@@ -15,14 +11,8 @@ class App extends Component {
     return (
       <Provider store={store}>
           <div className="app">
-            <aside className="app__sidebar">
-              <ScoreBoard/>
-            </aside>
             <main className="app__main">
-              <StartGame className="app__layer"/>
-              <Board className="app__layer"/>
             </main>
-            <AI/>
           </div>
       </Provider>
     );
---
+++ src/jest-setup.js
@@ -0,0 +1,66 @@
+// Make Enzyme functions available in all test files without importing
+import { shallow, render, mount } from 'enzyme';
+import React from 'react';
+
+global.shallow = shallow;
+global.render = render;
+global.mount = mount;
+
+// Fail tests on any warning
+console.error = message => {
+    throw new Error(message);
+};
+
+/**
+ * Function to mock an Enzyme component.
+ * @param enzymeRender {function} render function.
+ * @param Component {JSXComponent} component type to mock.
+ * @param componentProps {Object} optional component properties.
+ * @returns Enzyme Component.
+ */
+global.mockComponent = function (enzymeRender, Component, componentProps = {}) {
+
+    let jsxElement = <Component {...componentProps} />;
+    const enzymeComponent = enzymeRender(jsxElement);
+
+    return {
+        componentProps,
+        enzymeComponent
+    }
+};
+
+/**
+ * Function to create data driven tests against poly component configurations
+ * @param Component {JSXComponent} component type to mock.
+ * @param configurations {Array of objects} An object containing the test configuration for each test case. Each object within this array must have a 'description' and 'props' property.
+ * @returns nothing
+ */
+global.assertComponentProperties = function (Component, configurations) {
+
+    const expectedProperties = ['description', 'props'];
+
+    configurations.forEach( (testCase) => {
+
+        expectedProperties.forEach((property) => {
+            if(!testCase.hasOwnProperty(property)) {
+                throw new Error(`Error: assertComponentProperties 'configuration' property does not have required '${property}' property.`);
+            }
+        });
+
+        const { enzymeComponent } = mockComponent(shallow, Component, testCase.props);
+
+        it(testCase.description, () => {
+            expect(enzymeComponent).toMatchSnapshot();
+        });
+    });
+};
+
+global.mockResponse = function (status, statusText, response) {
+    return new window.Response(response, {
+        status: status,
+        statusText: statusText,
+        headers: {
+            'Content-type': 'application/json'
+        }
+    });
+};
