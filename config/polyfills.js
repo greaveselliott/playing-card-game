eb8bc852f0564c73625e792011d65372fb2e5d1f
Elliott-Greaves <greaveselliott@me.com>
2017-11-08 01:48:31 +0000

    IE 11 polyfill for array.from

--- config/polyfills.js
+++ config/polyfills.js
@@ -23,3 +23,14 @@ window.matchMedia = window.matchMedia || function() {
     removeListener: function() {}
   };
 };
+
+/**
+* @license MIT, GPL, do whatever you want
+* @requires polyfill: Array.prototype.slice fix {@link https://gist.github.com/brettz9/6093105}
+*/
+if (!Array.from) {
+  Array.from = function (object) {
+      'use strict';
+      return [].slice.call(object);
+  };
+}
